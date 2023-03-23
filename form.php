73a48b424313650fcf856a0741815a4d441754e7
Aline LEGENDRE <legendrealine@gmail.com>
2023-03-23 13:40:57 +0100

    quete 2 : les formulaires en PHP

--- form.php
+++ form.php
@@ -1,3 +1,28 @@
+<?php
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
+        $errors[] = "Le prénom est obligatoire";
+    }
+    if(!isset($_POST['lastname']) || trim($_POST['lastname']) === '') {
+        $errors[] = "Le nom est obligatoire";
+    }
+    if(!isset($_POST['telnumber']) || trim($_POST['telnumber']) === '') {
+        $errors[] = "Le numéro de téléphone est obligatoire";
+    }
+    if(!isset($_POST['email']) || trim($_POST['email'] === '') || filter_var('email', FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "L'adresse mail est non valide";
+    }
+    if(!isset($_POST['subject']) || trim($_POST['subject']) === '') {
+        $errors[] = "Le sujet du message est obligatoire";
+    }
+    if(!isset($_POST['message']) || trim($_POST['message']) === '') {
+        $errors[] = "Le message est obligatoire";
+    }
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,33 +33,44 @@
     <title>Formulaire</title>
 </head>
 <body>
+
+<?php
+if (count($errors) > 0) :
+    foreach ($errors as $error) :?>
+        <li><?= $error ?></li>
+        </ul>
+<?php
+    endforeach;
+endif;
+?>
+
 <form  action="thanks.php"  method="post">
     <div>
         <label  for="firstname">Prénom :</label>
-        <input  type="text"  id="firstname"  name="firstname">
+        <input  type="text"  id="firstname"  name="firstname" required>
     </div>
     <div>
         <label  for="lastname">Nom :</label>
-        <input  type="text"  id="lastname"  name="lastname">
+        <input  type="text"  id="lastname"  name="lastname"required>
     </div>
     <div>
         <label  for="telnumber">Numéro de téléphone :</label>
-        <input  type="tel"  id="telnumber"  name="telnumber">
+        <input  type="tel"  id="telnumber"  name="telnumber" required>
     </div>
     <div>
         <label  for="email">Courriel :</label>
-        <input  type="email"  id="email"  name="email">
+        <input  type="email"  id="email"  name="email" required>
     </div>
     <div>
         <label  for="subject">Sujet :</label>
-        <select id="subject" name="subject">
+        <select id="subject" name="subject" required>
             <option value="votre commande">votre commande</option>
             <option value="votre facture">votre facture</option>
         </select>
     </div>
     <div>
         <label  for="message">Message :</label>
-        <textarea  id="message"  name="message"></textarea>
+        <textarea  id="message"  name="message" required></textarea>
     </div>
     <div  class="button">
         <button  type="submit">Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -1,3 +1,28 @@
+<?php
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
+        $errors[] = "Le prénom est obligatoire";
+    }
+    if(!isset($_POST['lastname']) || trim($_POST['lastname']) === '') {
+        $errors[] = "Le nom est obligatoire";
+    }
+    if(!isset($_POST['telnumber']) || trim($_POST['telnumber']) === '') {
+        $errors[] = "Le numéro de téléphone est obligatoire";
+    }
+    if(!isset($_POST['email']) || trim($_POST['email'] === '') || filter_var('email', FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "L'adresse mail est non valide";
+    }
+    if(!isset($_POST['subject']) || trim($_POST['subject']) === '') {
+        $errors[] = "Le sujet du message est obligatoire";
+    }
+    if(!isset($_POST['message']) || trim($_POST['message']) === '') {
+        $errors[] = "Le message est obligatoire";
+    }
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,8 +33,21 @@
     <title>Thanks</title>
 </head>
 <body>
-<p>Merci <?= $_POST['firstname'] . " " . $_POST['lastname']; ?> de nous avoir contacté à propos de <?= $_POST['subject']; ?>.</p>
-<p>Un de nos conseillers vous contactera soit à l’adresse  <?= $_POST['email']?> ou par téléphone au <?= $_POST['telnumber'] ?> dans les plus brefs délais pour traiter votre demande : </p>
-<p> <?= $_POST['message'] ?></p>
+<?php
+if (empty($errors)) : ?>
+    <p>Merci <?= $_POST['firstname'] . " " . $_POST['lastname']; ?> de nous avoir contacté à propos de <?= $_POST['subject']; ?>.</p>
+    <p>Un de nos conseillers vous contactera soit à l’adresse  <?= $_POST['email']?> ou par téléphone au <?= $_POST['telnumber'] ?> dans les plus brefs délais pour traiter votre demande : </p>
+    <p> <?= $_POST['message'] ?></p>
+    <ul>
+    <?php else :
+        foreach ($errors as $error) :?>
+        <li><?= $error ?></li>
+    </ul>
+
+<?php
+        endforeach;
+    endif;
+?>
+
 </body>
 </html>
